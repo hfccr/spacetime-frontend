1f915b368c69fb162e8a0a6fbe77c5cd54a5fba9
hfccr <hfccr@outlook.com>
2023-02-06 01:58:31 +0530

    Revert spheron changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  trailingSlash: true,
-  assetPrefix: isProd ? "https://spacetime-frontend-fa412f.spheron.app/" : "",
 };
 
 module.exports = nextConfig;
