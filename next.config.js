31e0ca3ccc46b3db11a722d3ab01837d18af9d68
hfccr <hfccr@outlook.com>
2023-02-06 01:54:15 +0530

    Add asset path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -8,6 +9,7 @@ const nextConfig = {
     path: "",
   },
   trailingSlash: true,
+  assetPrefix: isProd ? "https://spacetime-frontend-fa412f.spheron.app/" : "",
 };
 
 module.exports = nextConfig;
